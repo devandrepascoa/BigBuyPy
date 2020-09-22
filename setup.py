b3e5a2f16d0aed445edd9d4ee3fb97f2471cfbb5
André Páscoa <andre@pascoa.org>
2020-09-22 09:33:22 +0100

    Updated configs and workflows

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 import setuptools
+import re
+import os.path as op
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("BigBuyPy/__init__.py", "r") as fh:
+    version_file = fh.read()
+
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    version_file,
+                    re.MULTILINE).group(1)
+print(version)
 setuptools.setup(
-    name="andrepascoa", # Replace with your own username
-    version="0.0.1",
+    name="BigBuyPy",  # Replace with your own username
+    version=version,
     author="André Páscoa",
     author_email="andre@pascoa.org",
     description="Big Buy connector for python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/devandrepascoa/BigBuyPy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
