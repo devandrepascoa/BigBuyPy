e9eddcce1feb2c83f3dfda4674e2090b196a947f
André Páscoa <andre@pascoa.org>
2020-09-22 07:30:25 +0100

    Modified testing framework

--- manager.py
+++ manager.py
@@ -7,13 +7,3 @@ if __name__ == '__main__':
     # run tests with unittest-xml-reporting and output to $CIRCLE_TEST_REPORTS on CircleCI or test-reports locally
     xmlrunner.XMLTestRunner(output=os.environ.get(
         'CIRCLE_TEST_REPORTS', 'test-reports')).run(tests)
-    if COV:
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
-        COV.erase()
